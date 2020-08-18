8120ebad319b1895c178845eb8f53c13731429da
Kalil Justin <kalilkjustin@gmail.com>
2020-08-17 23:00:48 -0300

    Publisher

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,10 +14,12 @@ public class BootStrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -39,5 +43,24 @@ public class BootStrapData implements CommandLineRunner {
 
         System.out.println("Started in BootStrap");
         System.out.println("Number of Books: " + bookRepository.count());
+
+        Publisher altaBooks = new Publisher("Alta Books",
+                "R. Viúva Cláudio, 291",
+                "Rio de Janeiro",
+                "RJ",
+                "20970-031");
+
+        publisherRepository.save(altaBooks);
+
+        Publisher wrox = new Publisher("Wrox",
+                "Rua 123",
+                "São Alegre",
+                "SA",
+                "12345-222");
+
+        publisherRepository.save(wrox);
+
+        System.out.println("Publishers:");
+        System.out.println("Number of publishers: " + publisherRepository.count());
     }
 }
