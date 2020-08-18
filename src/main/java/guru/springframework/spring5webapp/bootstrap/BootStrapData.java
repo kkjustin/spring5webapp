904bac6f1073dcc70c4e8a18ea11470557519c01
Kalil Justin <kalilkjustin@gmail.com>
2020-08-17 23:24:49 -0300

    Publisher and books relation

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -25,25 +25,6 @@ public class BootStrapData implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        Author eric = new Author("Eric", "Evans");
-        Book ddd = new Book("Domain Driven Design", "123123");
-        eric.getBooks().add(ddd);
-        ddd.getAuthors().add(eric);
-
-        authorRepository.save(eric);
-        bookRepository.save(ddd);
-
-        Author rod = new Author("Rod", "Johnson");
-        Book noEJB = new Book("J2EE Development without EJB", "393939765");
-        rod.getBooks().add(noEJB);
-        noEJB.getAuthors().add(rod);
-
-        authorRepository.save(rod);
-        bookRepository.save(noEJB);
-
-        System.out.println("Started in BootStrap");
-        System.out.println("Number of Books: " + bookRepository.count());
-
         Publisher altaBooks = new Publisher("Alta Books",
                 "R. Viúva Cláudio, 291",
                 "Rio de Janeiro",
@@ -60,7 +41,34 @@ public class BootStrapData implements CommandLineRunner {
 
         publisherRepository.save(wrox);
 
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Driven Design", "123123");
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+        ddd.setPublisher(altaBooks);
+        altaBooks.getBooks().add(ddd);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+        publisherRepository.save(altaBooks);
+
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB", "393939765");
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+        noEJB.setPublisher(wrox);
+        wrox.getBooks().add(noEJB);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+        publisherRepository.save(wrox);
+
+        System.out.println("Started in BootStrap");
+        System.out.println("Number of Books: " + bookRepository.count());
+
         System.out.println("Publishers:");
         System.out.println("Number of publishers: " + publisherRepository.count());
+        System.out.println("Alta Books Size = " + altaBooks.getBooks().size());
+        System.out.println("Worx Size = " + wrox.getBooks().size());
     }
 }
