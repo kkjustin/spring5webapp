a6df22b852086b5d9d176a969ee6c917f5971d96
Kalil Justin <kalilkjustin@gmail.com>
2020-08-28 09:39:11 -0300

    Author Added

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -13,7 +13,7 @@ public class Author {
     private Long id;
 
     private String firstName;
-    private String lastname;
+    private String lastName;
 
     @ManyToMany(mappedBy = "authors")
     private Set<Book> books = new HashSet<>();
@@ -21,9 +21,9 @@ public class Author {
     public Author() {
     }
 
-    public Author(String firstName, String lastname) {
+    public Author(String firstName, String lastName) {
         this.firstName = firstName;
-        this.lastname = lastname;
+        this.lastName = lastName;
     }
 
     public Long getId() {
@@ -42,12 +42,12 @@ public class Author {
         this.firstName = firstName;
     }
 
-    public String getLastname() {
-        return lastname;
+    public String getLastName() {
+        return lastName;
     }
 
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public Set<Book> getBooks() {
@@ -63,7 +63,7 @@ public class Author {
         return "Author{" +
                 "id=" + id +
                 ", firstName='" + firstName + '\'' +
-                ", lastname='" + lastname + '\'' +
+                ", lastname='" + lastName + '\'' +
                 ", books=" + books +
                 '}';
     }
